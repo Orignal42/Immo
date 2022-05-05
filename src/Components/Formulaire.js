b26a0f606da571eafcdcc892155345f8b7ebe7c0
Orignal42 <rbarriquand@gmail.com>
2022-05-05 17:03:55 +0200

    4eme depot

--- src/Components/Formulaire.js
+++ src/Components/Formulaire.js
@@ -1,7 +1,9 @@
 import React from "react";
+
 import axios from 'axios';
+
 import "../App.css";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 class Formulaire extends React.Component {
   constructor(props) {
     super(props);
@@ -10,17 +12,20 @@ class Formulaire extends React.Component {
       description: "",
       size: "",
       floor: "",
-      image: "",
+      image: null,
       price: "",
       address: "",
       postcode: "",
       city: "",
       room: "",
-      selectedFile: null
+      // selectedFile: null,
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
+  onFileChange = event => {
+    this.setState({ image: event.target.files[0] });
+        };
 
   handleChange(e) {
     let name = e.target.name;
@@ -31,139 +36,185 @@ class Formulaire extends React.Component {
     });
   }
 
-  handleSubmit(e) {
-   
-    fetch(`http://127.0.0.1:8000/api/property/add`, {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json; charset=UTF-8"
-            },
-      body:
-        JSON.stringify(this.state),
-      }).then((result)=>{
-        result.json().then((res)=>{
-          console.warn('res',res)
-          console.log(JSON.stringify(this.state))      
-          }).catch(err=>{
-            console.log(err)
-        })
-      })
-      //pour la remise à zero des inputs et empecher plusieurs envoie de suite
-      this.setState({
-        title: "",
-        description: "",
-        size: "",
-        floor: "",
-        image: "",
-        price: "",
-        address: "",
-        postcode: "",
-        city: "",
-        room: "",
-      });
-  }
+  handleSubmit = () => {
+    
+    // Create an object of formData
+    const formData = new FormData();
+  
+    // Update the formData object
+    formData.append(
+      "image",
+      this.state.image,
+      this.state.image.name,
+      )
+      formData.append(
+      'title',this.state.title
+      )
+      formData.append(
+      'description',this.state.description
+      )
+      formData.append(
+      'floor',this.state.floor
+      )
+      formData.append(
+      'size',this.state.size
+      )
+      formData.append(
+        'room',this.state.room
+        )
+      formData.append(
+      'price',this.state.price
+      )
+      formData.append(
+      'address',this.state.address
+      )
+      formData.append(
+      'postcode',this.state.postcode
+      )
+      formData.append(
+      'city',this.state.city
+    );
+    console.log(this.state.image.name);
+    console.log(this.state);
+      // Details of the uploaded file
+      // Request made to the backend api
+      // Send formData object
+      axios.post("http://127.0.0.1:8000/api/property/add", formData);
+    console.log("c'est fait");
+    console.log(formData);
+  };
+  
+  // File content to be displayed after
+  // file upload is complete
+
+  // handleSubmit(e) {
+  //   fetch(`http://127.0.0.1:8000/api/property/add`, {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-type": "application/json; charset=UTF-8",
+  //     },
+  //     body: 
+  //     // JSON.stringify(this.state),
+  //      this.state,
+
+  //   }).then((result) => {
+  //     result
+  //       .json()
+  //       .then((res) => {
+  //         console.warn("res", res);
+  //         console.log(JSON.stringify(this.state));
+  //       })
+  //       .catch((err) => {
+  //         console.log(err);
+  //       });
+  //   });
+  //   this.setState({
+  //     title: "",
+  //     description: "",
+  //     size: "",
+  //     floor: "",
+  //     image: "",
+  //     price: "",
+  //     address: "",
+  //     postcode: "",
+  //     city: "",
+  //     room: "",
+  //   });
+  // }
+
+
 
   render() {
     return (
       <div>
-        
-          <label htmlFor="title">Titre</label>
-          <input
-            type="text"
-            id="title"
-            name="title"
-            value={this.state.title}
-            onChange={this.handleChange}
-          />
-          <label htmlFor="description">Description</label>
-          <input
-            type="text"
-            id="description"
-            name="description"
-            value={this.state.description}
-            onChange={this.handleChange}
-          />
-          <label htmlFor="floor">Etage</label>
-          <input
-            type="number"
-            id="floor"
-            name="floor"
-            value={this.state.floor}
-            onChange={this.handleChange}
-            
-          />
-          <label htmlFor="size">Surface</label>
-          <input
-            type="number"
-            id="size"
-            name="size"
-            value={this.state.size}
-            onChange={this.handleChange}
-          />
-          <label htmlFor="image">Image</label>
-          <input
-            type="text"
-            id="image"
-            name="image"
-            value={this.state.image}
-            onChange={this.handleChange}
-          />
-          <label htmlFor="room">Piece</label>
-          <input
-            type="number"
-            id="room"
-            name="room"
-            value={this.state.room}
-            onChange={this.handleChange}
-          />
-          <label htmlFor="price">Prix</label>
-          <input
-            type="number"
-            id="price"
-            name="price"
-            min='100'
-            max='1000'
-            value={this.state.price}
-            onChange={this.handleChange}
-          />
-          <label htmlFor="address">Adresse</label>
-          <input
-            type="text"
-            id="address"
-            name="address"
-            value={this.state.address}
-            onChange={this.handleChange}
-          />
+        <label htmlFor="title">Titre</label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          value={this.state.title}
+          onChange={this.handleChange}
+        />
+        <label htmlFor="description">Description</label>
+        <input
+          type="text"
+          id="description"
+          name="description"
+          value={this.state.description}
+          onChange={this.handleChange}
+        />
+        <label htmlFor="floor">Etage</label>
+        <input
+          type="number"
+          id="floor"
+          name="floor"
+          value={this.state.floor}
+          onChange={this.handleChange}
+        />
+        <label htmlFor="size">Surface</label>
+        <input
+          type="number"
+          id="size"
+          name="size"
+          value={this.state.size}
+          onChange={this.handleChange}
+        />
+        <label htmlFor="image">Image</label>
+        {/* Input pour telecharger l'image */}
+        <input type="file" onChange={this.onFileChange} />
+        <label htmlFor="room">Piece</label>
+        <input
+          type="number"
+          id="room"
+          name="room"
+          value={this.state.room}
+          onChange={this.handleChange}
+        />
+        <label htmlFor="price">Prix</label>
+        <input
+          type="number"
+          id="price"
+          name="price"
+          min="100"
+          max="1000"
+          value={this.state.price}
+          onChange={this.handleChange}
+        />
+        <label htmlFor="address">Adresse</label>
+        <input
+          type="text"
+          id="address"
+          name="address"
+          value={this.state.address}
+          onChange={this.handleChange}
+        />
 
-          <label htmlFor="postcode">Code postale</label>
-          <input
-            type="number"
-            id="postcode"
-            name="postcode"
-            min='1000'
-            max='9999'
-            value={this.state.postcode}
-            onChange={this.handleChange}
-          />
+        <label htmlFor="postcode">Code postale</label>
+        <input
+          type="number"
+          id="postcode"
+          name="postcode"
+          min="1000"
+          max="9999"
+          value={this.state.postcode}
+          onChange={this.handleChange}
+        />
 
-          <label htmlFor="city">Ville</label>
-          <input
-            type="text"
-            id="city"
-            name="city"
-            value={this.state.city}
-            onChange={this.handleChange}
-          />
-          <button onClick={this.handleSubmit}>Soumettre</button>
- 
+        <label htmlFor="city">Ville</label>
+        <input
+          type="text"
+          id="city"
+          name="city"
+          value={this.state.city}
+          onChange={this.handleChange}
+        />
+        <button onClick={this.handleSubmit}>Soumettre</button>
 
         {JSON.stringify(this.state)}
-        <li><Link to="/">Accueil</Link></li>
-
+        <li>
+          <Link to="/">Accueil</Link>
+        </li>
       </div>
-
-
-
     );
   }
 }
